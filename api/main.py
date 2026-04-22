e5275706f3f68cf3eacac957f6a36610ed130269
Shreya Singh <shreya@Shreyas-MacBook-Air.local>
2026-04-22 12:08:02 +0530

    revert: remove Railway deployment changes

--- api/main.py
+++ api/main.py
@@ -15,7 +15,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://shreyaa-15.github.io", "https://*.vercel.app"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
